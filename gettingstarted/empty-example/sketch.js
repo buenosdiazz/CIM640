a79cba8abb9e841896c14e74e8f966d2507e0b8f
Caro <carolinadiaz017@gmail.com>
2017-10-05 16:35:14 -0400

    make face

--- gettingstarted/empty-example/sketch.js
+++ gettingstarted/empty-example/sketch.js
@@ -1,10 +1,29 @@
+
+var posX = 0;
+var posY = 0;  
+
 function setup(){
-    createCanvas(500,500);
+    createCanvas(500,1000);
     background(125,121,232);
+    posX = width/2;
+    posY = height/2;
 }
 
 
-function draw(){
-    ellipse(250,250,100,100);
-    
+function draw(){  
+    stroke(0);
+    strokeWeight(.4);
+    fill("pink");
+    ellipse(posX, posY,100,100);
+    fill("blue");
+    rect(posX- 25, posY+10,50,20);
+    fill ("#FFF");
+    ellipse(posX-10, posY-15, 10, 40);
+    ellipse(posX+10, posY-15, 10, 40);
+    fill(0);
+    ellipse(posX-10, posY-10, 10, 10);
+    ellipse(posX+10, posY-10, 10, 10);
+    stroke("blue");
+    strokeWeight(5);
+    point(posX +10, posY+10)
 }
