d5864b4f53f1ae3319d5a1f752a4fe17848681e5
Caro <carolinadiaz017@gmail.com>
2017-11-16 07:09:12 -0500

    hw

--- homework/hw5/empty-example/sketch.js
+++ homework/hw5/empty-example/sketch.js
@@ -1,33 +1,52 @@
 var interfaceItems = [];
 var brushSize = 10;
+var brushColor= "black";
 
 function setup(){
     createCanvas (400,400)
 
     interfaceItems.push(new interface(100,100,50,color(255,0,0)));
     interfaceItems.push(new interface(30,100,50,color(0,255,0)));
+    interfaceItems.push(new interface(170,100,50,color(0,0,255)));
+    interfaceItems.push(new interface(240,100,50,color(153,51,255)));
 }
 
 function draw (){
-    fill(0);
-    ellipse(mouseX,mouseY,brushSize,brushSize);
+    ellipse(mouseX,mouseY,brushSize,brushSize,fill(brushColor),stroke(brushColor));
 
   interfaceItems[0].check();
     interfaceItems[0].display();
       interfaceItems[1].check();
     interfaceItems[1].display();
+       interfaceItems[2].check();
+    interfaceItems[2].display();
+    interfaceItems[3].check();
+    interfaceItems[3].display();
     //console.log(interfaceItems[0].check());
 }
 
 function mousePressed(){
     if(interfaceItems[0].check() == true){
-        console.log("pressed red button");
+        console.log("pressed green button");
         brushSize++;
     }
 
        if(interfaceItems[1].check() == true){
         console.log("pressed red button");
         brushSize--;
+    }
+    if(interfaceItems[2].check() == true){
+        console.log("pressed blue button");
+        brushColor="blue";
+    }
+     if(interfaceItems[3].check() == true){
+        console.log("pressed blue button");
+        brushColor="blue";
+    }
+
+     if(interfaceItems[3].check() == true){
+        console.log("pressed blue button");
+        brushColor=color(153,51,255);
     }
 }
 
